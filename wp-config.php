291b0c28db343b409431cd6c1d77eb02dad75439
Quan Vu <quan.vu@evizi.com>
2019-11-19 09:29:22 +0700

    Update config server mysql

--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'rosiehouse_db' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'quanvh81' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', '271081' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'choxeoto.com' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
